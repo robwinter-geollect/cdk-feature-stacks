a27e80cb61d07ea5b3179a78f93c9160211ed53a
robwinter-geollect <robwinter@geollect.com>
2024-06-12 13:28:42 +0100

    Create Fargate for deployment

--- infrastructure/lib/stacks/example-app/example-app-stack.ts
+++ infrastructure/lib/stacks/example-app/example-app-stack.ts
@@ -1,17 +1,41 @@
-import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
+import path from 'path';
+import { aws_ec2, aws_ecs } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
 import { GeollectStack, GeollectStackProps } from '../geollect-stack';
 
-type Props = GeollectStackProps;
 
-export class ExampleAppStack extends GeollectStack {
+const rootPath = path.posix.join(__dirname, '..', '..', '..', '..', '..', '..', '..');
+
+type Props = GeollectStackProps
+
+export class HelloWorldStack extends GeollectStack {
   constructor(scope: Construct, id: string, { environmentId, ...props }: Props) {
     super(scope, id, { environmentId, ...props });
 
-    new LambdaFunction(this, 'ExampleFunction', {
-      code: Code.fromInline('exports.handler = async function(event, context) { return "hello world"; }'),
-      handler: 'index.handler',
-      runtime: Runtime.NODEJS_18_X,
+    const vpc = aws_ec2.Vpc.fromLookup(this, 'Vpc', {
+      isDefault: true,
+    });
+    const cluster = new aws_ecs.Cluster(this, 'HelloWorldCluster', {
+      vpc,
+    });
+
+    const taskDefinition = new aws_ecs.FargateTaskDefinition(this, 'HelloWorldTaskDefintion');
+
+    taskDefinition.addContainer('HelloWorldContainer', {
+      image: aws_ecs.ContainerImage.fromDockerImageAsset(new DockerImageAsset(this, 'HelloWorldImage', {
+        directory: rootPath,
+        file: path.posix.join('projects', 'hello_world', 'Dockerfile')
+      })),
+      memoryLimitMiB: 512,
+      cpu: 256,
+    });
+
+    new aws_ecs.FargateService(this, 'HelloWorldService', {
+      cluster,
+      taskDefinition,
+      // desiredCount: 1,
+      // assignPublicIp: true,
     });
   }
 }
--- infrastructure/lib/stacks/pipeline/constructs/deployment-stages/example-deployment.ts
+++ infrastructure/lib/stacks/pipeline/constructs/deployment-stages/example-deployment.ts
@@ -1,6 +1,6 @@
 import { Stage, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { ExampleAppStack } from '../../../example-app/example-app-stack';
+import { HelloWorldStack } from '../../../example-app/example-app-stack';
 
 type EnvironmentStageProps = StageProps & {
   environmentId: string;
@@ -10,8 +10,7 @@ export class ExampleDeploymentStage extends Stage {
   constructor(scope: Construct, id: string, props: EnvironmentStageProps) {
     super(scope, id, props);
 
-    new ExampleAppStack(this, `ExampleAppStack`, {
-      description: 'Example App Stack from the CDK template repository',
+    new HelloWorldStack(this, "HelloWorldStack", {
       environmentId: props.environmentId,
       tags: {
         Service: 'example-app',
