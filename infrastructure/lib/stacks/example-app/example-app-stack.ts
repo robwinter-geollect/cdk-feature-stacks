cac84f562faf7b6817007033de9ae354606c15c1
robwinter-geollect <robwinter@geollect.com>
2024-06-13 13:11:02 +0100

    remoev console logs

--- infrastructure/lib/stacks/example-app/example-app-stack.ts
+++ infrastructure/lib/stacks/example-app/example-app-stack.ts
@@ -23,9 +23,7 @@ import { GeollectStack, GeollectStackProps } from '../geollect-stack';
 
 
 const rootPath = path.posix.join(__dirname, '..', '..', '..', '..');
-console.log('rootPath: ', rootPath);
 const file = path.posix.join('projects', 'hello_world');
-console.log('file: ', file);
 type Props = GeollectStackProps
 
 const directory = path.posix.join(rootPath, file);
@@ -33,21 +31,17 @@ const directory = path.posix.join(rootPath, file);
 export class HelloWorldStack extends GeollectStack {
   constructor(scope: Construct, id: string, { environmentId, ...props }: Props) {
     super(scope, id, { environmentId, ...props });
-    console.log('vpc');
     const vpc = new aws_ec2.Vpc(this, 'HelloWorldVpc', {
     });
-    console.log('cluster');
 
     const cluster = new aws_ecs.Cluster(this, 'HelloWorldCluster', {
       vpc,
     });
 
-    console.log('imageAsset');
     const imageAsset = new DockerImageAsset(this, 'HelloWorldImage', {
       directory,
     });
 
-    console.log('taskDefinition');
     const taskDefinition = new aws_ecs.FargateTaskDefinition(this, 'HelloWorldTaskDefintion');
     taskDefinition.addContainer('HelloWorldContainer', {
       image: aws_ecs.ContainerImage.fromDockerImageAsset(imageAsset),
@@ -55,7 +49,6 @@ export class HelloWorldStack extends GeollectStack {
       cpu: 256,
     });
 
-    console.log('service');
     new aws_ecs.FargateService(this, 'HelloWorldService', {
       cluster,
       taskDefinition,
