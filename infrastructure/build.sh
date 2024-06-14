1aa50d8cd4843fec47d4d8a9cd339929264b5181
robwinter-geollect <robwinter@geollect.com>
2024-06-14 11:56:49 +0100

    chore: Update build.sh to change directory to infrastructure before running npm commands

--- infrastructure/build.sh
+++ infrastructure/build.sh
@@ -6,11 +6,5 @@ npm ci
 npm run test
 npm run lint
 set -e
-# Build docker images and push to ECR and then retrieve the image URI and pass to the context of the synth step
-aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-echo Build started on `date`
-
-docker-compose -f ../docker-compose.yaml build
-docker-compose -f ../docker-compose.yaml push
 
 npx cdk synth --context branch=${BRANCH} --context environmentId=${ENV_ID}
